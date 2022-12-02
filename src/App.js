9a7e407d617be3ddbd52ef335801c8f018041964
Kamil Jochym <kamil.jochym@warwick.ac.uk>
2022-12-02 10:37:29 +0100

    changed api

--- src/App.js
+++ src/App.js
@@ -1,71 +1,73 @@
-import './App.css';
+import './App.css'
 import React, {useState} from 'react'
 import axios from 'axios'
 
 function App() {
-
-  const [ quoteInfo, setQuoteInfo ] = useState('')
-  const [ choices, setChoices ] = useState([])
+  const [quoteInfo, setQuoteInfo] = useState('')
+  const [choices, setChoices] = useState([])
 
   const generateQuote = () => {
-    document.getElementsByClassName('choice0')[0].style.backgroundColor = "";
-    document.getElementsByClassName('choice1')[0].style.backgroundColor = "";
-    document.getElementsByClassName('choice2')[0].style.backgroundColor = "";
-    document.getElementsByClassName('choice3')[0].style.backgroundColor = "";
+    document.getElementsByClassName('choice0')[0].style.backgroundColor = ''
+    document.getElementsByClassName('choice1')[0].style.backgroundColor = ''
+    document.getElementsByClassName('choice2')[0].style.backgroundColor = ''
+    document.getElementsByClassName('choice3')[0].style.backgroundColor = ''
 
     const tempChoices = []
     axios
-      .get('http://localhost:3001/quote')
-      .then(response => {
+      .get('/quote')
+      .then((response) => {
         const data = response.data
         setQuoteInfo(data[0])
-        tempChoices.push(data[0][2])    
-        return axios.get('http://localhost:3001/quote')
+        console.log(data[0].quote)
+        tempChoices.push(data[0].author)
+        return axios.get('/quote')
       })
-      .then(response => {
-        tempChoices.push(response.data[0][2])    
-        return axios.get('http://localhost:3001/quote')
+      .then((response) => {
+        tempChoices.push(response.data[0].author)
+        return axios.get('/quote')
       })
-      .then(response => {
-        tempChoices.push(response.data[0][2])    
-        return axios.get('http://localhost:3001/quote')
+      .then((response) => {
+        tempChoices.push(response.data[0].author)
+        return axios.get('/quote')
       })
-      .then(response => {
-        tempChoices.push(response.data[0][2])   
-        let shuffledtempChoices = tempChoices.sort(() => (Math.random() > .5) ? 1 : -1); 
+      .then((response) => {
+        tempChoices.push(response.data[0].author)
+        let shuffledtempChoices = tempChoices.sort(() => (Math.random() > 0.5 ? 1 : -1))
         setChoices(shuffledtempChoices)
       })
   }
 
   const checkCorrect = (idx) => {
-    const correctAnswer = quoteInfo[2]
+    const correctAnswer = quoteInfo.author
     if (correctAnswer === choices[idx]) {
-      console.log("correct");
-      document.getElementsByClassName('choice'+idx)[0].style.backgroundColor = "green";
-
+      console.log('correct')
+      document.getElementsByClassName('choice' + idx)[0].style.backgroundColor = 'green'
+    } else {
+      console.log('wrong')
+      document.getElementsByClassName('choice' + idx)[0].style.backgroundColor = 'red'
     }
-    else {
-      console.log("wrong");
-      document.getElementsByClassName('choice'+idx)[0].style.backgroundColor = "red";
-    }
-
   }
-  
 
   return (
-    <div className="wrapper">
+    <div className='wrapper'>
       <button onClick={generateQuote}>next quote</button>
-      <div className="quote">
-        {quoteInfo[1]}
-      </div>
-      <div className="choices-wrapper">
-          <div className="choice0" onClick={() => checkCorrect(0)}>{choices[0]}</div>
-          <div className="choice1" onClick={() => checkCorrect(1)}>{choices[1]}</div>
-          <div className="choice2" onClick={() => checkCorrect(2)}>{choices[2]}</div>
-          <div className="choice3" onClick={() => checkCorrect(3)}>{choices[3]}</div>
+      <div className='quote'>{quoteInfo.quote}</div>
+      <div className='choices-wrapper'>
+        <div className='choice0' onClick={() => checkCorrect(0)}>
+          {choices[0]}
+        </div>
+        <div className='choice1' onClick={() => checkCorrect(1)}>
+          {choices[1]}
+        </div>
+        <div className='choice2' onClick={() => checkCorrect(2)}>
+          {choices[2]}
+        </div>
+        <div className='choice3' onClick={() => checkCorrect(3)}>
+          {choices[3]}
+        </div>
       </div>
     </div>
   )
 }
 
-export default App;
+export default App
